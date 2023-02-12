4ba8a942a8fb8d16050989536abf8ba2db651380
final_nan <final_nan@yeah.net>
2023-02-12 23:00:34 +0800

    apple redesined sanity-server

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,1 +1,6 @@
-export const schemaTypes = []
+import product from './product'
+import category from './category'
+import blockContent from './blockContent'
+import {user, account} from 'next-auth-sanity/schemas'
+
+export const schemaTypes = [product, category, blockContent, user, account]
